93a66d7309a44e16611a1facde29cad7ec60fb8d
Jorav <johan@ravnborg.se>
2024-08-11 19:26:02 +0200

    Fixed R-reader file

--- dataReader.R
+++ dataReader.R
@@ -1,3 +1,3 @@
 print (1)
-my_data <- read.table("C:/Users/Skuta/Documents/free-time/Programmering/monogame-repos/OBB_CD_Comparison/OBB_CD_Comparison/bin/Debug/netcoreapp3.1/elapsedTimes.txt")
+my_data <- read.table("C:/Users/Skuta/Documents/free-time/Programmering/monogame-repos/OBB_CD_Comparison/OBB_CD_Comparison/bin/Debug/net6.0/MSE.txt")
 plot(as.numeric(sub(",", ".", my_data[,1], fixed = TRUE)))
